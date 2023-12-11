9bb51aaed2a2ed71b58279a52ede1a9752caca82
Tquandoo <quantrungdo2401@gmail.com>
2023-12-11 11:07:33 +0700

    relative_absolute_import

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,8 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import CssBaseline from "@mui/material/CssBaseline";
 import { Experimental_CssVarsProvider as CssVarsProvider } from "@mui/material/styles";
-import { ThemeProvider } from "@mui/material/styles";
-import theme from "./theme"; // Đảm bảo bạn có file theme hợp lệ
+// import { ThemeProvider } from "@mui/material/styles";
+import theme from "~/theme"; // Đảm bảo bạn có file theme hợp lệ
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
