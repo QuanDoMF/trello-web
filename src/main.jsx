33562447356faedf92933af8687313849d4ed6fb
Tquandoo <quantrungdo2401@gmail.com>
2024-04-07 09:00:51 +0700

    axios_call_api

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,4 @@
-// import React from "react";
+import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import CssBaseline from "@mui/material/CssBaseline";
@@ -7,12 +7,12 @@ import { Experimental_CssVarsProvider as CssVarsProvider } from "@mui/material/s
 import theme from "~/theme"; // Đảm bảo bạn có file theme hợp lệ
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  // <React.StrictMode>
-  <CssVarsProvider theme={theme}>
-    <CssBaseline />
-    <App />
-  </CssVarsProvider>
-  // </React.StrictMode>
+  <React.StrictMode>
+    <CssVarsProvider theme={theme}>
+      <CssBaseline />
+      <App />
+    </CssVarsProvider>
+  </React.StrictMode>
 );
 
 // Phần còn lại của component App của bạn không thay đổi
--- src/pages/Boards/_id.jsx
+++ src/pages/Boards/_id.jsx
@@ -3,13 +3,21 @@ import { Container } from "@mui/material";
 import AppBar from "~/components/AppBar/AppBar";
 import BoardBar from "./BoardBar/BoardBar";
 import BoardContent from "./BoardContent/BoardContent";
-import { mockData } from "~/apis/mock-data";
+import { useEffect, useState } from "react";
+import { fetchBoardDetailsAPI } from "~/apis";
 const Board = () => {
+  const [board, setBoard] = useState([])
+  useEffect(() => {
+    const boardId = '6607df5d6cb9760cc225f561'
+    fetchBoardDetailsAPI(boardId).then(board => {
+      setBoard(board)
+    })
+  }, [])
   return (
     <Container disableGutters maxWidth={true} sx={{ height: "100vh" }}>
       <AppBar />
-      <BoardBar board={mockData?.board} />
-      <BoardContent board={mockData?.board} />
+      <BoardBar board={board} />
+      <BoardContent board={board} />
     </Container>
   );
 };
